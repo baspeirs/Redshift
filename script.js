5faedf3450160c7c5b7695c67e4757c2391bb10e
Bryce Speirs <baspeirs@gmail.com>
2021-09-15 12:35:24 -0400

    moved skeleton application to git repository

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function()  {
+    $("#readHeader").fadeIn(2000);
+});
+
+setTimeout(() => {
+    $("#gameTitle").fadeIn(2000);
+}, 1500);
+
+setTimeout(() => {
+    $("#server").fadeIn(2000); 
+}, 3000);
