37bcc2c4c80661b75f1c59a9d8a1a5cd116c83ed
Bryce Speirs <baspeirs@gmail.com>
2021-10-30 15:50:53 -0400

    attempt to fix css and js error in production

--- Public/script.js
+++ Public/script.js
@@ -2,8 +2,6 @@ let group1;
 let group2;
 let rosterDataTypes;
 let specs;
-// import * as specs from "./specs.json";
-// const specs = require("./specs")
 
 $(document).ready(function () {
     $("#readHeader").fadeIn(2000);
@@ -28,50 +26,11 @@ fetch("./specs.json")
     .then(res => res.json())
     .then(data => specs = data)
 
-// const specs = {
-//     "Arcane": {
-//         "src": "./Icons/Arcane_mage.png",
-//         "alt": "Arcane Mage Icon"
-//     },
-//     "Frost": {
-//         "src": "./Icons/Frost_mage.png",
-//         "alt": "Frost Mage Icon"
-//     },
-//     "Fire": {
-//         "src": "./Icons/Frost_mage.png",
-//         "alt": "Fire Mage Icon"
-//     },
-//     "Protection": {
-//         "src": "./Icons/Prot_Warior.png",
-//         "alt": "Prot Warior Icon"
-//     },
-//     "Arms": {
-//         "src": "./Icons/Arms_Warior.png",
-//         "alt": "Prot_Warior Icon"
-//     },
-//     "Fury": {
-//         "src": "./Icons/Fury_Warior.png",
-//         "alt": "Prot_Warior Icon"
-//     },
-//     "Assassination": {
-//         "src": "./Icons/Assassination_Rogue",
-//         "alt": "Assassination Rogue Icon"
-//     },
-//     "Subtlety": {
-//         "src": "./Icons/Subtlety_Rogue.png",
-//         "alt": "Subtlety Rogue Icon"
-//     },
-//     "Combat": {
-//         "src": "",
-//         "alt": "Combat Rogue Icon"
-//     }
-// }
-
 getRoster().then(res => {
     rosterDataTypes = res.group1.values[0]
     group1 = res.group1.values
     group2 = res.group2.values
-
+    // either seems to work, leaving both to see if one ever fails
     renderGroup(group1, rosterDataTypes, 1);
     renderGroup(group2, rosterDataTypes, "2")
 });
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-    res.sendFile('./Public/index.html', {root: __dirname })
+    res.render('./Public/index.html', {root: __dirname })
 });
 
 require("dotenv").config();
