294c77c2b9d6b1f6bece0bf3633ed1293dc61922
Bryce Speirs <baspeirs@gmail.com>
2021-10-30 16:08:37 -0400

    spelling error

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const {google} = require("googleapis");
-const path = requre("path");
+const path = require("path");
 let returnData;
 // supported google apis
 const apis = google.getSupportedAPIs();
