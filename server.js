56bfee125508cf1fcad35496e79685da18a2bef3
Bryce Speirs <baspeirs@gmail.com>
2021-10-30 11:05:06 -0400

    trying res.sendFile instead of res.render

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-    res.render("index");
+    res.sendFile('./Public/index.html', {root: __dirname })
 });
 
 require("dotenv").config();
